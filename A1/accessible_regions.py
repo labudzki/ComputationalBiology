6c707e3181a664ceb33642675283b1433314dbae
Andrea Labudzki <andrealabudzki@gmail.com>
2025-04-16 08:49:50 +0200

    accessible regions plot

--- A1/accessible_regions.py
+++ A1/accessible_regions.py
@@ -11,80 +11,47 @@ import matplotlib.pyplot as plt
 import os
 
 
+# Function defining line v6 = v1 + D
 def func_v1_d(x, d):
     return x+d
 
-save_plots=False
+# Commands to save plots to desktop
+save_plots=True
 desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
 
 # Parameters
-# D_vals = [0.2, 0.5]
-D = 1.2
-vmax = 0.8
-x_vals = np.linspace(0, 1.5, 20)
+D = 0.7
+vmax = 0.3
 
-#y = x+D
+# Defining x values
+x_lim = 1.1
+x_vals = np.linspace(0, x_lim, 50)
+
+#getting y = x+D values
 y_vals = func_v1_d(x_vals, D)
 
     
-
+#Plotting figure 
 plt.figure()
 plt.plot(x_vals, y_vals, label=f'v6 = v1 + D')
-plt.plot(x_vals, x_vals, label=f'v6 = v1')
+
 
 # vmax line
 plt.axhline(y=vmax, color='black', linestyle='--', label=f'v6 = vmax')
 
-plt.axvline(x=D, color='black', label=f'x=D')
+# Filling in feasible region
+plt.fill_between(x_vals[y_vals < vmax], y_vals[y_vals < vmax], vmax, color='orange', alpha=0.5)
 
-plt.fill_between(x_vals[y_vals < vmax], y_vals[y_vals < vmax], vmax, color='orange', alpha=0.3)
-
-plt.xlim(0, 1.5)
-plt.ylim(0, 1.5)
+plt.xlim(0, x_lim)
+plt.ylim(0, x_lim)
 plt.xlabel('v1')
 plt.ylabel('v6')
 plt.legend()
 plt.grid(True)
-plt.title(f'Accessible region, D = {D}')
+plt.title(f'Accessible region, D = {D}, vmax = {vmax}')
 if save_plots:
-    plt.savefig(os.path.join(desktop_path, f"accessible_regions_D_{D}_plot.png"), dpi=300)
+    plt.savefig(os.path.join(desktop_path, f"accessible_regions_D_{D}_vmax_{vmax}_plot.png"), dpi=300)
     plt.close()
 else:
     plt.show()
-
-
-
-# for D in D_vals:
-#     y_vals1 = x_vals + D
-#     y_vals2 = x_vals 
-
-#     # Keep only points where both x and y are in [0,1]
-#     mask = (y_vals1 >= 0) & (y_vals1 <= 1)
-#     x = x_vals[mask]
-#     y1 = y_vals1[mask]
-#     y2 = y_vals2[mask]
-
-#     plt.figure()
-#     plt.plot(x, y1, label=f'v6 = v1 + D')
-#     plt.plot(x, y2, label=f'v6 = v1')
-    
-#     # vmax line
-#     plt.axhline(y=vmax, color='black', linestyle='--', label=f'v6 = vmax')
-
-#     # Shade area between y and vmax where y < vmax
-#     mask_below = y1 < vmax
-#     plt.fill_between(x[mask_below], y1[mask_below], vmax, color='orange', alpha=0.3)
-
-#     plt.xlim(0, 1)
-#     plt.ylim(0, 1)
-#     plt.xlabel('v1')
-#     plt.ylabel('v6')
-#     plt.legend()
-#     plt.grid(True)
-#     plt.title(f'Accessible region')
-#     if save_plots:
-#         plt.savefig(os.path.join(desktop_path, f"accessible_regions_D_{D}_plot.png"), dpi=300)
-#         plt.close()
-#     else:
-#         plt.show()
 
